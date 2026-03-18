100ff8ca63750ee029d2fb5c7ec0bc10e7b044f2
Ivan <mail@mail.com>
2026-03-18 18:14:26 +0300

    first atotest

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def culct(c):
-    a = 3
-    b = 2
-    print(a+b+c)
+import driver
+import time
 
-culct(c=int(input('vvedite3')))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://yandex.by/")
+search_box = driver.find_element(By.NAME, "text")
+search_query = "хабр
+search_box.send_keys(search_query)
+search_box.send_keys(Keys.RETURN)
+time.sleep(2)
+link = driver.find_element(By.LINK_TEXT, "Все статьи в порядке публикации на Хабре")
+link.click()
+time.sleep(2)
+tabs = driver.window_handles
+driver.switch_to.window(tabs[-1])
+login_button = driver.find_element(By.LINK_TEXT, "Войти")
+login_button.click()
+time.sleep(2)
