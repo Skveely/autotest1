ff015ac1a68ebfbd659ce0f065da464ff3d051e5
Ivan <qvikli2609@gmail.com>
2026-03-20 00:23:02 +0300

    написанный тест на открытие вкладки shorts

---
+++ test1_main.py
@@ -0,0 +1,44 @@
+import driver
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    return driver
+
+def test_open_shorts(driver):
+# Открыть ссылку
+    driver.get("https://yandex.by/")
+# Найти строку поиска
+    search_box = driver.find_element(By.NAME, "text")
+# Ввести "YouTube"
+    search_query = "YouTube"
+    search_box.send_keys(search_query)
+    search_box.send_keys(Keys.RETURN)
+# Переходим на YouTube
+    link = driver.find_element(By.LINK_TEXT, "YouTube")
+    link.click()
+# Переключаемся на новую вкладку
+    tabs = driver.window_handles
+    driver.switch_to.window(tabs[-1])
+ # Ищем Shorts
+    login_button = driver.find_element(By.LINK_TEXT, "Shorts")
+    login_button.click()
+    time.sleep(2)
+    assert "shorts" in driver.current_url.lower()
+# Имитируем нажатие стрелки вниз 5 раз
+   # for i in range(5):
+  #      ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+   # time.sleep(2)
+
+
+
+
